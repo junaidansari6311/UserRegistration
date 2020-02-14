376b8038d021c22cffca19781c7772d8c4babe40
junaidansari6311 <junaidansari6311@gmail.com>
2020-02-14 11:03:41 +0530

    ADDED: validation for first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,1 +1,16 @@
+#!/bin/bash -x
+shopt -s extglob
 echo "WELCOME TO USER REGISTRATION"
+read -p "Enter first name: " firstName
+
+function checkFirstName (){
+	validFirstName="^[A-Z]{1}[a-z]{3,}$"
+	if [[ $1 =~ $validFirstName ]]
+	then
+		echo "First Name is valid"
+	else
+		echo "First Name is invalid"
+	fi
+}
+
+checkFirstName $firstName
