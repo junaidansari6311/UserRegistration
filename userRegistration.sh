4e2fb96ba0d09d5bc517e7c28feab41c19d71aaf
junaidansari6311 <junaidansari6311@gmail.com>
2020-02-14 17:07:34 +0530

    ADDED: validation for password according to rule 1

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 shopt -s extglob
 echo "WELCOME TO USER REGISTRATION"
+
 read -p "Enter first name : " firstName
 read -p "Enter last name : " lastName
 read -p "Enter email id : " email
 read -p "Enter mobile no : " mobileNo
+read -p "Enter password : " password
 
 #Valid pattern for first and last name
 validName="^[A-Z]{1}[a-z]{2,}$"
@@ -12,6 +14,8 @@ validName="^[A-Z]{1}[a-z]{2,}$"
 validEmail="^[A-Za-z]{3,}([-|+|.]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2,4}*([.][A-Za-z]{2,4})?$"
 #valid pattern for mobile no
 validMobileNo="^[0-9]{1,3}[[:space:]][0-9]{10}$"
+#valid pattern for password
+passwordRule1="^[A-Za-z]{8,}$"
 
 function validationOfUserInputs (){
 	local userInput=$1
@@ -28,3 +32,4 @@ validationOfUserInputs $firstName $validName
 validationOfUserInputs $lastName $validName
 validationOfUserInputs $email $validEmail
 validationOfUserInputs "$mobileNo" $validMobileNo
+validationOfUserInputs $password $passwordRule1
