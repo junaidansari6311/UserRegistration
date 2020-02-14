e823753e235009a321702d5b6dabbacf494ef50f
junaidansari6311 <junaidansari6311@gmail.com>
2020-02-14 16:01:11 +0530

    ADDED: validation for mobile number

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,10 +4,14 @@ echo "WELCOME TO USER REGISTRATION"
 read -p "Enter first name : " firstName
 read -p "Enter last name : " lastName
 read -p "Enter email id : " email
+read -p "Enter mobile no : " mobileNo
+
 #Valid pattern for first and last name
 validName="^[A-Z]{1}[a-z]{2,}$"
 #Valid pattern for email
 validEmail="^[A-Za-z]{3,}([-|+|.]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2,4}*([.][A-Za-z]{2,4})?$"
+#valid pattern for mobile no
+validMobileNo="^[0-9]{1,3}[[:space:]][0-9]{10}$"
 
 function validationOfUserInputs (){
 	local userInput=$1
@@ -23,3 +27,4 @@ function validationOfUserInputs (){
 validationOfUserInputs $firstName $validName
 validationOfUserInputs $lastName $validName
 validationOfUserInputs $email $validEmail
+validationOfUserInputs "$mobileNo" $validMobileNo
